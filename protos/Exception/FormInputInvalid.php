4bf3bb7a8ed93fd15afcf4b0a9b5961d597e86a2
Gu Weigang <guweigang@outlook.com>
2016-04-27 13:37:07 +0800

    update from falconplus

--- protos/Exception/FormInputInvalid.php
+++ protos/Exception/FormInputInvalid.php
@@ -3,9 +3,9 @@ namespace Common\Protos\Exception;
 /**
  * 此类由代码自动生成，请不要修改
  */
-class NeedLogin extends \PhalconPlus\Base\Exception
+class FormInputInvalid extends \PhalconPlus\Base\Exception
 {
-    protected $code = 10003;
-    protected $message = '需要登录';
+    protected $code = 20001;
+    protected $message = '__DATA__%s';
     protected $level = 6;
 }
